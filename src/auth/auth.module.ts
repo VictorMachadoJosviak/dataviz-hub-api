2067e9b5457f5e29aae00a6f98e34a1705fb7509
VictorMachadoJosviak <victormjosviak@gmail.com>
2024-03-24 20:26:02 -0300

    feat: add jwt authentication

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,33 @@
+import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { jwtConstants } from './constants/jwt';
+import { AuthController } from './constrollers/auth/auth.controller';
+import { JwtAuthGuard } from './guards/jwt-auth/jwt-auth.guard';
+import { AuthService } from './services/auth/auth.service';
+import { JwtStrategy } from './strategies/jwt/jwt.strategy';
+import { LocalStrategy } from './strategies/local/local.strategy';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [
+    AuthService,
+    LocalStrategy,
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
+  controllers: [AuthController],
+})
+export class AuthModule {}
---
+++ src/auth/constrollers/auth/auth.controller.ts
@@ -0,0 +1,36 @@
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '../../../users/services/user/users.service';
+import { Public } from '../../decorators/public/public';
+import { JwtAuthGuard } from '../../guards/jwt-auth/jwt-auth.guard';
+import { LocalAuthGuard } from '../../guards/local-auth/local-auth.guard';
+import { AuthService } from '../../services/auth/auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login')
+  async login(@Body() user: User) {
+    return this.authService.login(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @Public()
+  @Get()
+  findAll() {
+    return [];
+  }
+}
