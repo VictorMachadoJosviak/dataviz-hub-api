9149f71ab93f172a8b44c6d16023a9989d077846
VictorMachadoJosviak <victormjosviak@gmail.com>
2024-03-26 23:54:49 -0300

    chore: set port from env

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupSwagger } from './common/swagger/setup-swagger';
 
+const PORT = process.env.PORT || 3000;
+
 export async function buildApp(app: INestApplication) {
   app.setGlobalPrefix('api');
   setupSwagger(app);
+
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -21,6 +25,6 @@ async function bootstrap() {
 
   buildApp(app);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 bootstrap();
