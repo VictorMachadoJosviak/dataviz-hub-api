6f75ca4b9496d6e410be5f784e6d9c54e08c7ef1
VictorMachadoJosviak <victormjosviak@gmail.com>
2024-03-29 12:55:08 -0300

    chore: move mikro orm cli and migrations from dev dependencies to dependencies

--- prepare.sh
+++ prepare.sh
@@ -1,8 +1,2 @@
 echo "Installing Nest JS CLI"
 npm i -g @nestjs/cli
-
-echo "Installing Mikro ORM CLI"
-npm i @mikro-orm/cli
-
-echo "Installing Mikro ORM Migrations CLI"
-npm i @mikro-orm/migrations
