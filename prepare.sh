9fda6a92880e1915f50312eedf77a6e2741e5806
VictorMachadoJosviak <victormjosviak@gmail.com>
2024-03-29 12:49:35 -0300

    chore: add prepare script

---
+++ prepare.sh
@@ -0,0 +1,8 @@
+echo "Installing Nest JS CLI"
+npm i -g @nestjs/cli
+
+echo "Installing Mikro ORM CLI"
+npm i -g @mikro-orm/cli
+
+echo "Installing Mikro ORM Migrations CLI"
+npm i -g @mikro-orm/migrations
